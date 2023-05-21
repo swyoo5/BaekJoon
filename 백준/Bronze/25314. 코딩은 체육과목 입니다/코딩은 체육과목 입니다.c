86d37885366c9b127d62799891e2669291d4a13d
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-05-21 16:17:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int byte;
+    
+    scanf("%d", &byte);
+    
+    for (int i = 0; i < byte/4; i++){
+        printf("long ");
+    }
+    printf("int\n");
+    return 0;
+}
