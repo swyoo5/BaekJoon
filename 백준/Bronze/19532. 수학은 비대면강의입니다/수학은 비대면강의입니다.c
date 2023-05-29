26970cf836e9d11bb8a6934b2f860beb24977663
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-05-29 12:56:52 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, c, d, e, f;
+    int x, y;
+    
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+    for (x = -999; x <= 999; x++){
+        for (y = -999; y <= 999; y++){
+            if (a * x + b * y == c && d * x + e * y == f){
+                printf("%d %d", x, y);
+            }
+        }
+    }
+    return 0;
+}
