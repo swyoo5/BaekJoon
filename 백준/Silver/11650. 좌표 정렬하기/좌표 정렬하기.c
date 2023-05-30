46530c4ef0dd240591287842ed43509b75f8d472
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-05-30 15:23:46 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 76 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int x, y;
+} coord;
+
+int compare(const void *a, const void *b);
+
+int main(void) {
+    int n;
+    
+    scanf("%d", &n);
+    coord arr[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }    
+    
+    qsort(arr, n, sizeof(coord), compare);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+    
+    return 0;
+}
+
+int compare(const void *a, const void *b) {
+    coord A = *(coord *) a;
+    coord B = *(coord *) b;
+    
+    if (A.x > B.x) {
+        return 1;
+    } else if (A.x == B.x) {
+        if (A.y > B.y) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+    return -1;
+}
