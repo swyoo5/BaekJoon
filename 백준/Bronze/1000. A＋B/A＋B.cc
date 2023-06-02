18713b96eebb5e7e2cbaa4466fb0491c282b9379
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-06-02 16:11:18 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+    int A, B;
+    std::cin >> A;
+    std::cin >> B;
+    
+    int result = A + B;
+    std::cout << result << std::endl;
+    
+    return 0;
+}
