27eaa5a5d209026a9eb8793aeb241bcadebdaa07
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-06-07 17:00:13 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N, index = 0, diff = 1, numerator, denominator;
+
+    cin >> N;
+    while (1) {
+        if (N <= index + diff) {
+            if (diff % 2 == 0) {
+                numerator = N - index;
+                denominator = (diff + 1) - (N - index);
+                break;
+            } else {
+                numerator = (diff + 1) - (N - index);
+                denominator = N - index;
+                break;
+            }
+        }
+        index += diff;
+        diff++;
+    }
+    cout << numerator << "/" << denominator << endl;
+    return 0;
+}
