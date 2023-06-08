a35353a0e5a6ba82bb52a736c238203f9e75fdd7
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-06-08 15:22:13 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int width, height;
+    
+    cin >> width >> height;
+    
+    int area = width * height;
+    cout << area << "\n";
+    
+    return 0;
+}
