f6ba768d83cc37b12b2a9528d74d551f083211c6
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-06-08 15:37:11 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    
+    cin >> n;
+    
+    long result = (long) 4 * n;
+    cout << result << "\n";
+    
+    return 0;
+}
