0d0d89275fdfa142125c95e1554b7257a057e456
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-07-04 11:51:00 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -8,17 +8,13 @@ int main(void) {
     cin >> input;
     
     int result1 = 0, result2 = 0;
-    
-    if (input[0] == '0') result1++;
-    else result2++;
-    
     for (int i = 0; i < input.length(); i++) {
-        if (input[i] == '0' && input[i + 1] == '1') {
-            result2++;
-        }
-        
-        if (input[i] == '1' && input[i + 1] == '0') {
-            result1++;
+        if (input[i] != input[i + 1]) {
+            if (input[i] == '0') {
+                result1++;
+            } else {
+                result2++;
+            }
         }
     }
     int result = min(result1, result2);
