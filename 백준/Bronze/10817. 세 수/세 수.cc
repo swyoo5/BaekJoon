9312230c0b6f3c794f8a16cb82d715fd524c98ec
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-07-30 15:19:11 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int a[3];
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+
+    sort(a, a + 3);
+    cout << a[1] << endl;
+    return 0;
+}
