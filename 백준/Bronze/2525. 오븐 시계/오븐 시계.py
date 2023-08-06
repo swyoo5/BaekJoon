db63fcdad1b4bb90656fd270aaffcef41284e768
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-06 19:33:12 +0900

    [Bronze III] Title: 오븐 시계, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,12 @@
+hour, minute = map(int, input().split())
+add_min = int(input())
+
+minute += add_min
+if (minute >= 60) :
+    hour += minute / 60
+    minute %= 60
+    
+    if (hour >= 24) :
+        hour -= 24
+        
+print(int(hour), int(minute))
