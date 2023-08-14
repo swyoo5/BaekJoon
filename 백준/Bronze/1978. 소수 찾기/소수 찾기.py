d293b72e50f1beab74815473d4b4d8547983910a
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-14 21:27:12 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,13 +1,21 @@
-N = int(input())
+case = int(input())
 number_list = map(int, input().split())
+
+def is_prime(num) :
+    if num < 2 : 
+        return False
+    elif num == 2 : 
+        return True
+    else :
+        for i in range(2, int(num ** 0.5) + 1) :
+            if (num % i == 0) :
+                return False
+            
+    return True
+
 prime = 0
 for num in number_list :
-    is_prime = True
-    if num >= 2 :
-        for i in range(2, num) :
-            if num % i == 0 :
-                is_prime = False
-        if (is_prime) :
-            prime += 1
-            
+    if is_prime(num) :
+        prime += 1
+        
 print(prime)
