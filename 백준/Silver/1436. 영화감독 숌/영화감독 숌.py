38d1bd9853878c735c83032307a8fae2194b7f75
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-16 18:19:52 +0900

    [Silver V] Title: 영화감독 숌, Time: 576 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+index = 1
+movie = 666
+
+while True :
+    if '666' in str(movie) :
+        if (index == n) :
+            print(movie)
+            break
+        else :
+            index += 1
+    movie += 1
