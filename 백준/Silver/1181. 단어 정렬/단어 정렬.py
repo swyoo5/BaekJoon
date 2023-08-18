88f83ae5d38b91fde780e8ae0f018e3900553f9d
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-18 17:40:08 +0900

    [Silver V] Title: 단어 정렬, Time: 800 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,12 @@
+N = int(input())
+words = []
+
+for _ in range(N) :
+    words.append(input())
+    
+words = list(set(words))
+words.sort()
+words.sort(key = len)
+
+for word in words :
+    print(word)
