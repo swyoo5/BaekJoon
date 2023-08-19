d254bf68d6dff9958ec89a47a9d1f48c71814313
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-19 17:44:07 +0900

    [Bronze II] Title: 다이얼, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,10 +1,10 @@
 word = input()
 dial = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
+
 time = 0
-for unit in dial :
-    for alphabet in unit :
-        for i in word :
-            if i == alphabet :
-                time += dial.index(unit) + 3
-                
+for alphabet in word :
+    for button in dial :
+        if alphabet in button :
+            time += (dial.index(button) + 3)
+            
 print(time)
