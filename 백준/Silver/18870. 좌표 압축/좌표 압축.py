5c2f087c3faa3c3ad8922340feac214601f77eda
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-19 18:27:02 +0900

    [Silver II] Title: 좌표 압축, Time: 1848 ms, Memory: 154800 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,8 +1,8 @@
 N = int(input())
-x = list(map(int, input().split()))
+x_list = list(map(int, input().split()))
 
-x_sort = sorted(list(set(x)))
+x_sort = sorted(list(set(x_list)))
 x_dict = {x_sort[i] : i for i in range(len(x_sort))}
 
-for i in x :
+for i in x_list :
     print(x_dict[i])
