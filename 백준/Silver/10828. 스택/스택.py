99969cdcfc20e589d92adc4a325439ef587cafd1
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-08-20 14:49:28 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,25 +1,27 @@
 import sys
 
-case = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+
 stack = []
-for _ in range(case) :
+for _ in range(n) :
     command = list(sys.stdin.readline().split())
+    length = len(stack)
     if (command[0] == "push") :
         stack.append(command[1])
     elif (command[0] == "pop") :
-        if (len(stack) == 0) :
+        if (length == 0) :
             print(-1)
         else :
             print(stack.pop())
     elif (command[0] == "size") :
-        print(len(stack))
+        print(length)
     elif (command[0] == "empty") :
-        if (len(stack) == 0) :
+        if (length == 0) :
             print(1)
         else :
             print(0)
     elif (command[0] == "top") :
-        if (len(stack) == 0) :
+        if (length == 0) :
             print(-1)
         else :
             print(stack[-1])
