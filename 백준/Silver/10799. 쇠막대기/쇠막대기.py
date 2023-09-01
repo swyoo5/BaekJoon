bb42be9fe75bca74129180c14f683beac6c8df9a
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-09-02 00:57:16 +0900

    [Silver II] Title: 쇠막대기, Time: 64 ms, Memory: 32472 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,18 @@
+parenthesis = list(input())
+answer = 0
+stack = []
+
+for i in range(len(parenthesis)) :
+    if (parenthesis[i] == '(') :
+        stack.append('(')
+        
+    else :
+        if (parenthesis[i - 1] == '(') :
+            stack.pop()
+            answer += len(stack)
+            
+        else :
+            stack.pop()
+            answer += 1
+            
+print(answer)
