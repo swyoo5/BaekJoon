77beb706bcedf1c7d70881017cb3a06f6e39c819
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-10-15 23:48:36 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,19 +1,21 @@
 def solution(today, terms, privacies):
-    d ={}
     answer = []
-    today_lst = list(map(int,today.split('.'))) # 오늘 날짜 리스트로 변환
+    terms_dict = {}
+    cur_year, cur_month, cur_day = map(int, today.split('.'))
     
-    for term in terms: # 약관종류와 개월수 딕셔너리로 저장
-        n, m = term.split()
-        d[n] = int(m)*28 # 한 달 일 수 곱해줌
-    
-    for i in range(len(privacies)):
-        date, s = privacies[i].split()
-        date_lst = list(map(int, date.split('.'))) # 수집일자 리스트로 변환
-        year = (today_lst[0] - date_lst[0])*336 # 연도 차이에 일 수 곱해주기
-        month = (today_lst[1] - date_lst[1])*28 # 달 수 차이에 일 수 곱해주기
-        day = today_lst[2] - date_lst[2]
-        total = year+month+day
-        if d[s] <= total:
-            answer.append(i+1)
+    for term in terms :
+        kind, period = term.split(' ')
+        terms_dict[kind] = int(period) * 28
+        
+    for i in range(len(privacies)) :
+        pri_date, pri_kind = privacies[i].split(' ')
+        pri_year, pri_month, pri_day = map(int, pri_date.split('.'))
+        
+        year = (cur_year - pri_year) * 28 * 12
+        month = (cur_month - pri_month) * 28
+        day = (cur_day - pri_day)
+        
+        total = year + month + day
+        if terms_dict[pri_kind] <= total :
+            answer.append(i + 1)
     return answer
