1e27c8006b8362a73e4b702acb5074b67c03a0d4
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-10-15 23:56:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -2,19 +2,19 @@ def solution(today, terms, privacies):
     answer = []
     terms_dict = {}
     cur_year, cur_month, cur_day = map(int, today.split('.'))
-    
+
     for term in terms :
         kind, period = term.split(' ')
         terms_dict[kind] = int(period) * 28
-        
-    for i in range(len(privacies)) :
+
+    for i in range(len(privacies)) : # 원소의 인덱스로 받음
         pri_date, pri_kind = privacies[i].split(' ')
         pri_year, pri_month, pri_day = map(int, pri_date.split('.'))
-        
-        year = (cur_year - pri_year) * 28 * 12
+
+        year = (cur_year - pri_year) * 336
         month = (cur_month - pri_month) * 28
         day = (cur_day - pri_day)
-        
+
         total = year + month + day
         if terms_dict[pri_kind] <= total :
             answer.append(i + 1)
