5c6081c69bf5465ce31018f8996fa34510f3067d
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-10-18 15:00:54 +0900

    [level 1] Title: 소수 찾기, Time: 2878.55 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 1
+    if n == 2 : return answer
+
+    for i in range(3, n + 1, 2) :
+        is_prime = True
+        j = 0
+        for j in range(2, int(i ** 0.5 + 1)) :
+            if i % j == 0 :
+                is_prime = False
+                break
+        if is_prime :
+            answer += 1
+    return answer
