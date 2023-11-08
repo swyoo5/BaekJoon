4348fba8eeff8077bf7e61381264afbb27596ad3
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-11-08 12:44:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,6 +1,7 @@
-def solution(food):
-    answer = ''
-    for i, num in enumerate(food) :
-        answer += str(i) * (num // 2)
+def solution(food) :
+    answer = ""
+    for i in range(1, len(food)) :
+        answer += str(i) * (food[i] // 2)
     answer = answer + '0' + answer[::-1]
+    
     return answer
