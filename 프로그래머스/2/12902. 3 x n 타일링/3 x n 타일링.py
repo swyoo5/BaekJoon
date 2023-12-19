fb68f0ea2286bedc56fa44904b1ba7c6919a3a4f
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2023-12-19 14:58:27 +0900

    [level 2] Title: 3 x n 타일링, Time: 279.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -2,7 +2,7 @@ def solution(n):
     mod = 1000000007
     dp = [0 for _ in range(n + 1)]
     dp[2], dp[4] = 3, 11
-    for i in range(4, n + 1, 2) :
+    for i in range(6, n + 1, 2) :
         dp[i] = dp[i - 2] * 3 + 2
         for j in range(i - 4, -1, -2) :
             dp[i] += dp[j] * 2
