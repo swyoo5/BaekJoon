d7c794a348d7c851bc056a49778614f728b6a08c
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-06-03 16:21:37 +0900

    [Bronze I] Title: 단어 공부, Time: 228 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,14 +1,15 @@
-word = input().upper()
-alphabet = [0] * 26
+S = input() # mississipi
+string = S.upper() # 대문자로 변환
+listOfRepeat = [0]*26 # 반복횟수를 체크할 리스트
+# a-z까지 몇번 반복했는지
+for i in string :
+    indexOfAlphabet = ord(i)-65 # 몇번째 인덱스의 알파벳인지 (A = 0 ,B = 1...)
+    listOfRepeat[indexOfAlphabet] += 1 # 알파벳 하나씩 사용될 때마다 1씩 추가
 
-for alp in word :
-    if 'A' <= alp <= 'Z' :
-        alphabet[ord(alp) - ord('A')] += 1
-
-max_count = max(alphabet)
-if (alphabet.count(max_count) > 1) :
+if listOfRepeat.count(max(listOfRepeat)) > 1 :
     print('?')
 else :
-    max_index = alphabet.index(max(alphabet))
-    max_word = chr(max_index + ord('A'))
-    print(max_word)
+    # 가장 많이 반복된 횟수의 인덱스를 추출
+    indexOfMax = listOfRepeat.index(max(listOfRepeat)) 
+    maxAlphabet = chr(indexOfMax+65) # 아스키코드 이용해 알파벳으로 변환
+    print(maxAlphabet)
