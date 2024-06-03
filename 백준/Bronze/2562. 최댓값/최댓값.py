e61c99fa2eac3550a972cf9c4c478532ada6de95
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-06-03 16:42:56 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,8 +1,6 @@
 num_list = []
-
-for _ in range(9) :
+for i in range(9) :
     a = int(input())
     num_list.append(a)
     
-print(max(num_list))
-print(num_list.index(max(num_list)) + 1)
+print(max(num_list), num_list.index(max(num_list)) + 1)
