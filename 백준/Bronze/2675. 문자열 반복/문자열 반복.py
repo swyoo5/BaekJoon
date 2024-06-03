0698030db443a807ba43e903c9c46626cda28d15
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-06-03 17:22:35 +0900

    [Bronze II] Title: 문자열 반복, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,6 +1,8 @@
-case = int(input())
-for _ in range(case) :
-    num, word = input().split()
-    for w in word :
-        print(w * int(num), end = '')
-    print()
+num = int(input())
+for _ in range(num) :
+    result  = ""
+    r, s = map(str, input().split())
+    r = int(r)
+    for i in s :
+        result += i * r
+    print(result)
