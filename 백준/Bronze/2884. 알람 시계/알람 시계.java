9679772a31c902fe158ded1bb9fc804ac6220c92
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-08-17 16:50:09 +0900

    [Bronze III] Title: 알람 시계, Time: 172 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour;
+        int minute;
+        
+        hour = sc.nextInt();
+        minute = sc.nextInt();
+        
+        hour = ((minute - 45 < 0) ? hour - 1 : hour);
+        minute = ((minute - 45 < 0) ? minute + 15 : minute - 45);
+        hour = ((hour < 0) ? hour + 24 : hour);
+        
+        System.out.printf("%d %d", hour, minute);
+        
+        sc.close();
+    }
+}
