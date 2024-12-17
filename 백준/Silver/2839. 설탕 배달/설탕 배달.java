a671cb18dcb512cb9848c4665b9f80002afd6d34
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-17 22:21:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int weight = scanner.nextInt();
+        
+        int[] dp = new int[weight + 1];
+        
+        for (int i = 1; i <= weight; i++) {
+            dp[i] = -1;
+        }
+        
+        if (weight >= 3) {
+            dp[3] = 1;
+        }
+        
+        if (weight >= 5) {
+            dp[5] = 1;
+        }
+        
+        for (int i = 6; i <= weight; i++) {
+            if (dp[i - 3] != -1) {
+                dp[i] = dp[i - 3] + 1;
+            }
+            
+            if (dp[i - 5] != -1) {
+                dp[i] = dp[i - 5] + 1;
+            }
+            
+            if (dp[i] != -1 && dp[i - 3] != -1 && dp[i - 5] != -1) {
+                dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+            }
+        }
+        
+        System.out.println(dp[weight]);
+    }
+}
