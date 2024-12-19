aa78bca86ce5327d157c046fcda30162e2a12cae
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-20 07:54:39 +0900

    [Silver I] Title: 회의실 배정, Time: 1088 ms, Memory: 174560 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[][] meetings = new int[n][2];
+        
+        for (int i = 0; i < n; i++) {
+            meetings[i][0] = sc.nextInt();
+            meetings[i][1] = sc.nextInt();
+        }
+        
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+        
+        int count = 0;
+        int endTime = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (meetings[i][0] >= endTime) {
+                count++;
+                endTime = meetings[i][1];
+            }
+        }
+        System.out.println(count);
+    }
+}
