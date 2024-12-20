e9442e25be13c811410f157184594390da518a30
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-20 10:17:40 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 280 ms, Memory: 19004 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int T = scanner.nextInt();
+        
+        int[] coins = {25, 10, 5, 1};
+        for (int i = 0; i < T; i++) {
+            int money = scanner.nextInt();
+            int[] result = new int[4];
+            for (int j = 0; j < coins.length; j++) {
+                result[j] = money / coins[j];
+                money = money % coins[j];
+            }
+            
+            for (int j = 0; j < result.length; j++) {
+                System.out.print(result[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
