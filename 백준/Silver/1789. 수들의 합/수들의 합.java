8591d8371c776f73fe6cd390438defd49eabfd0c
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-20 10:46:32 +0900

    [Silver V] Title: 수들의 합, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        long n = scanner.nextLong();
+        
+        long sum = 0;
+        int result = 0;
+        for (int i = 1; ; i++) {
+            if (sum > n) {
+                result--;
+                break;
+            } else {
+                sum += i;
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
