7f539169ffd014d373a921709f829c0a49409904
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-20 09:40:38 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 168 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		String expression = scanner.next();
-		
-		scanner.close();
-		
-		String[] substract = expression.split("-");
-		
-		int result = 0;
-		for (int i = 0; i < substract.length; i++) {
-			int temp = 0;
-			String[] add = substract[i].split("\\+");
-			
-			for (int j = 0; j < add.length; j++) {
-				temp += Integer.parseInt(add[j]);
-			}
-			
-			if (i == 0) {
-				result += temp;
-			} else {
-				result -= temp;
-			}
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String expression = scanner.next();
+        
+        String[] substract = expression.split("-");
+        
+        int result = 0;
+        for (int i = 0; i < substract.length; i++) {
+            String[] add = substract[i].split("\\+");
+            int temp = 0;
+            
+            for (int j = 0; j < add.length; j++) {
+            	temp += Integer.parseInt(add[j]);    
+            }
+            
+            if (i == 0) {
+                result += temp;
+            } else {
+                result -= temp;
+            }
+        }
+        
+        System.out.println(result);
+    }
 }
