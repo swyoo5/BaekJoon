6a90f25b6f57f7e18eee18c1a6e302a3dd02b972
sang won Yoo (유상원) <119785794+swyoo5@users.noreply.github.com>
2024-12-20 09:47:09 +0900

    [Silver IV] Title: 보물, Time: 184 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+        }
+        
+        Arrays.sort(a);
+        Arrays.sort(b, Comparator.reverseOrder());
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += a[i] * b[i];
+        }
+        
+        System.out.println(result);
+    }
+}
